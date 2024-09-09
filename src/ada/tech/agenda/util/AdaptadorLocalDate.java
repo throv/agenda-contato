e8b5b716dd68de89178c9ae02151795bf6ac2959
throv <trv.vieira@gmail.com>
2024-09-09 11:23:34 -0300

    fix localdate adapter

--- src/ada/tech/agenda/util/AdaptadorLocalDate.java
+++ src/ada/tech/agenda/util/AdaptadorLocalDate.java
@@ -1,2 +1,24 @@
-package ada.tech.agenda.util;public class AdaptadorLocalDate {
+package ada.tech.agenda.util;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class AdaptadorLocalDate extends TypeAdapter<LocalDate> {
+
+    private final DateTimeFormatter formatadorLocalDate = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    @Override
+    public void write(JsonWriter jsonWriter, LocalDate localDate) throws IOException {
+        jsonWriter.value(localDate.format(formatadorLocalDate));
+    }
+
+    @Override
+    public LocalDate read(JsonReader jsonReader) throws IOException {
+        return LocalDate.parse(jsonReader.nextString(), formatadorLocalDate);
+    }
 }
